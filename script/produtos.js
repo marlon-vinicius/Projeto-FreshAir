9e3c1198758711a484de8786301cf0d25418cee7
Marlon Vinicius <marlon.vinicius2@gmail.com>
2023-05-19 18:17:08 -0300

    Listando os produtos

--- script/produtos.js
+++ script/produtos.js
@@ -3,9 +3,14 @@ const {createApp} = Vue
 createApp({
     data(){
         return {
-            produtos: [{nome:'Ventilador de mesa'},{nome: 'Ventilador de teto'},{nome: 'Ventilador de coluna'}, {nome: 'Ventilador de parede'}, {nome: 'Peças de reposição'}],
-            modelos: [{nome:'FAM-01'},{nome: 'FAM-01 Turbo'},{nome:'FAC-01'},{nome: 'FAC-01 Turbo'},{nome:'FAT-01'},{nome: 'FAC-01 Turbo'}, {nome:'FAP-01'},{nome: 'FAP-01 Turbo'},
-                      {nome:'FAM-02'},{nome: 'FAM-02 Turbo'},{nome:'FAC-02'},{nome: 'FAC-02 Turbo'},{nome:'FAT-02'},{nome: 'FAC-02 Turbo'}, {nome:'FAP-02'},{nome: 'FAP-02 Turbo'},],
+            filters: [{nome:'Ventilador de mesa'},{nome: 'Ventilador de teto'},{nome: 'Ventilador de coluna'}, {nome: 'Ventilador de parede'}, {nome: 'Peças de reposição'}],
+            produtos: [{nome:'FAM-01', img: '../img/ventilador.png'},{nome: 'FAM-01 Turbo', img: '../img/ventilador.png'},{nome:'FAC-01', img: '../img/ventilador.png'},
+                      {nome: 'FAC-01 Turbo', img: '../img/ventilador.png'},{nome:'FAT-01', img: '../img/ventilador.png'},{nome: 'FAC-01 Turbo', img: '../img/ventilador.png'},
+                      {nome:'FAP-01', img: '../img/ventilador.png'},{nome: 'FAP-01 Turbo', img: '../img/ventilador.png'},{nome:'FAM-02', img: '../img/ventilador.png'},
+                      {nome: 'FAM-02 Turbo', img: '../img/ventilador.png'},{nome:'FAC-02', img: '../img/ventilador.png', img: '../img/ventilador.png'},
+                      {nome: 'FAC-02 Turbo', img: '../img/ventilador.png'},{nome:'FAT-02', img: '../img/ventilador.png'},{nome: 'FAC-02 Turbo', img: '../img/ventilador.png'},
+                      {nome:'FAP-02', img: '../img/ventilador.png'},{nome: 'FAP-02 Turbo', img: '../img/ventilador.png'},],
+            
 
         }           
     }
