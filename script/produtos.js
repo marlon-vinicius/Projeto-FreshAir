f8da5b6c49cfd5b8fa7704e146de198b17073157
Marlon Vinicius <marlon.vinicius2@gmail.com>
2023-05-20 18:29:31 -0300

    Ajuste nos produtos

--- script/produtos.js
+++ script/produtos.js
@@ -4,14 +4,10 @@ createApp({
     data(){
         return {
             filters: [{nome:'Ventilador de mesa'},{nome: 'Ventilador de teto'},{nome: 'Ventilador de coluna'}, {nome: 'Ventilador de parede'}, {nome: 'Peças de reposição'}],
-            produtos: [{nome:'FAM-01', img: '../img/ventilador.png'},{nome: 'FAM-01 Turbo', img: '../img/ventilador.png'},{nome:'FAC-01', img: '../img/ventilador.png'},
-                      {nome: 'FAC-01 Turbo', img: '../img/ventilador.png'},{nome:'FAT-01', img: '../img/ventilador.png'},{nome: 'FAC-01 Turbo', img: '../img/ventilador.png'},
-                      {nome:'FAP-01', img: '../img/ventilador.png'},{nome: 'FAP-01 Turbo', img: '../img/ventilador.png'},{nome:'FAM-02', img: '../img/ventilador.png'},
-                      {nome: 'FAM-02 Turbo', img: '../img/ventilador.png'},{nome:'FAC-02', img: '../img/ventilador.png', img: '../img/ventilador.png'},
-                      {nome: 'FAC-02 Turbo', img: '../img/ventilador.png'},{nome:'FAT-02', img: '../img/ventilador.png'},{nome: 'FAC-02 Turbo', img: '../img/ventilador.png'},
-                      {nome:'FAP-02', img: '../img/ventilador.png'},{nome: 'FAP-02 Turbo', img: '../img/ventilador.png'},],
-            
-
+            produtos: [{nome:'FAM-01', img: '../img/ventilador.png'},{nome:'FAM-02', img: '../img/ventilador2.png'},
+                      {nome:'FAT-01', img: '../img/ventilador_de_teto.png'},{nome:'FAT-02', img: '../img/ventilador_de_teto2.png'},
+                      {nome:'FAC-01', img: '../img/ventilador_de_coluna.png'},{nome:'FAC-02', img: '../img/ventilador_de_coluna2.png'},
+                      {nome:'FAP-01', img: '../img/ventilador_parede.png'},{nome:'FAP-02', img: '../img/ventilador_parede2.png'},],
         }           
     }
    
